7b69b15e31e606c8ef79a92467d16383d8472947
Amanda Lee <leeandama87@gmail.com>
2023-03-07 20:44:18 -0800

    Added script back into profile.handlebars. Fixes error when creating a new post and adds it to the homepage.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { Comment, Post } = require('../models');
-const withAuth = require('../utils/auth');
 
 // Works
 router.get('/', async(req, res) => {
@@ -48,25 +47,6 @@ router.get('/post/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-// router.get('/profile', withAuth, async (req, res) => {
-//   try {
-//     // Find the logged in user based on the session ID
-//     const commentsData = await Comment.findByPk(req.session.userId, {
-//       attributes: { exclude: ['password'] },
-//       include: [{ model: Posts }],
-//     });
-
-//     const user = userData.get({ plain: true });
-
-//     res.render('profile', {
-//       ...user,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
